4541a15c15f35cbe304584479a0a58e76c48d9c4
YutaGoto <goto@rejob.co.jp>
2016-12-08 12:59:17 +0900

    mikujidesu

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ controller.hears(['お御籤', '御御籤', 'お神籤', '御神籤', 'おみく
 
     var name_from_msg = message.match[1];
 
-    var omikujiArray = ['吉' ,'小吉' ,'大吉' ,'中吉' ,'半吉' ,'末小吉' ,'末吉' ,'凶' ,'半凶' ,'小凶' ,'末凶' ,'大凶'];
+    var omikujiArray = ['吉' ,'小吉' ,'大吉' ,'中吉' ,'半吉' ,'末小吉' ,'末吉' ,'凶' ,'半凶' ,'小凶' ,'末凶' ,'大凶', '古', 'キチ', '区', '(๑•̀ㅂ•́)و✧'];
     var omikujiResult = omikujiArray[Math.floor(Math.random() * omikujiArray.length)];
 
     bot.reply(message, '*' + omikujiResult + '*');
