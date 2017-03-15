e2d25d7f163d5499ac6ef2e5b5545283929c38db
YutaGoto <goto@rejob.co.jp>
2017-03-15 15:53:28 +0900

    notify

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ controller.hears(['お知らせ'], 'direct_message,direct_mention,mention', func
 
     // bot.reply()で、botに発言をさせます。
     var notifyTalk = [
-        '`biz ○○` とつぶやくと、ファイナルファンタジー風に翻訳してくれます。'
+        '`biz ○○` とつぶやくと、意識高い系に翻訳してくれます。'
     ];
     var joinNotifyTalk = notifyTalk.join("\n");
     bot.reply(message, joinNotifyTalk);
@@ -72,6 +72,7 @@ controller.hears(['機能一覧'], 'direct_message,direct_mention,mention', func
         '`○○でニコニコ検索` と呼びかけると、ニコニコ動画検索します。○○は半角スペースでAnd検索できます。',
         '`○○って何` と呼びかけると、○○についてwikipedia検索します。',
         '`nomu ○○` とつぶやくと、ファイナルファンタジー風に翻訳してくれます。',
+        '`biz ○○` とつぶやくと、意識高い系に翻訳してくれます。',
         '`:excite: ○○` とつぶやくと、excite再翻訳してくれます。',
         '`:translate: ○○` とつぶやくと、excite翻訳してくれます。'
     ];
